5cd5f7a36d0c6c0b76658b01c26cea212dab1153
yu3san3 <orange12817@yahoo.co.jp>
2023-07-03 00:17:39 +0900

    ファイルの表示の部分を完成させた

--- TangoTestKun2/View/ContentView.swift
+++ TangoTestKun2/View/ContentView.swift
@@ -17,104 +17,39 @@ struct ContentView: View {
     @State private var isShowingVersionAlert = false
     @State private var isCheckingAnswers = false
     @State private var isImporting = false
-    @State private var isShowingExistingFileEditView = false
-    @State private var isShowingNewFileEditView = false
+    @State private var isShowingFileEditView = false
 
     var body: some View {
-        VStack {
-            header
-            TabView {
-                TangoTestView(
-                    tangoData: .constant(TangoParser.parse(tangoFile.text)),
-                    testType: .jp,
-                    isCheckingAnswers: $isCheckingAnswers
-                )
-                .tabItem {
-                    Image(systemName: "j.circle.fill")
-                    Text("日本語")
-                }
-                TangoTestView(
-                    tangoData: .constant(TangoParser.parse(tangoFile.text)),
-                    testType: .en,
-                    isCheckingAnswers: $isCheckingAnswers
-                )
-                .tabItem {
-                    Image(systemName: "e.circle.fill")
-                    Text("英語")
-                }
-            }
-        }
-    }
-}
-
-private extension ContentView {
-    var header: some View {
-        HStack {
-            titleText
-            Spacer()
-            fileMenu
-            .sheet(
-                isPresented: $isShowingExistingFileEditView,
-                content: {
-//                    FileEditView(tangoFile: nowEditingFile, editMode: .existingFile)
-                    Text("hello")
-                }
+        TabView {
+            TangoTestView(
+                tangoData: .constant(TangoParser.parse(tangoFile.text)),
+                testType: .jp,
+                isCheckingAnswers: $isCheckingAnswers
             )
-        }
-        .padding(.horizontal, 10)
-    }
-
-    var titleText: some View {
-        Text("単語テストくん")
-            .bold()
-            .padding(.vertical, 5)
-            .padding(.horizontal, 10)
-            .onTapGesture {
-                isShowingVersionAlert = true
+            .tabItem {
+                Image(systemName: "j.circle.fill")
+                Text("日本語")
             }
-            .alert("単語テストくん", isPresented: $isShowingVersionAlert) {
-                Button("OK") {}
-            } message: {
-                Text("\(appVersion) (\(appBuildNum))")
+            TangoTestView(
+                tangoData: .constant(TangoParser.parse(tangoFile.text)),
+                testType: .en,
+                isCheckingAnswers: $isCheckingAnswers
+            )
+            .tabItem {
+                Image(systemName: "e.circle.fill")
+                Text("英語")
             }
-    }
-
-    var fileMenu: some View {
-        Menu {
-            importTangoFileButton
-//            if !nowEditingFile.tangoData.isEmpty {
-                editExistingFileButton
-//            }
-            createNewFileButton
-        } label: {
-            Label("フォルダ", systemImage: "folder")
-                .labelStyle(.iconOnly)
-                .padding(.vertical, 7)
-                .padding(.horizontal, 14)
         }
-    }
-
-    var importTangoFileButton: some View {
-        Button(action: {
-            isImporting = true
-        }) {
-            Label("読み込み", systemImage: "arrow.down.doc")
-        }
-    }
-
-    var editExistingFileButton: some View {
-        Button(action: {
-            isShowingExistingFileEditView = true
-        }) {
-            Label("編集", systemImage: "pencil")
-        }
-    }
-
-    var createNewFileButton: some View {
-        Button(action: {
-            isShowingNewFileEditView = true
-        }) {
-            Label("新規作成", systemImage: "plus")
+        .navigationTitle("単語テストくん")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    isShowingFileEditView = true
+                }, label: {
+                    Label("編集", systemImage: "doc.text")
+                })
+            }
         }
     }
 }
